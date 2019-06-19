852f69a900d9f62a2e2cdf2fd6568c8b47f06ed0
Sayed-Shalan <sayedshalan22@gmail.com>
2019-06-19 20:51:37 +0200

    define basic classes-dataBinding etc..

--- app/src/main/java/com/sayed/intcoretest/utils/AppUtils.java
+++ app/src/main/java/com/sayed/intcoretest/utils/AppUtils.java
@@ -1,4 +1,30 @@
 package com.sayed.intcoretest.utils;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+
 public class AppUtils {
+
+    //Navigation on back-icon
+    public static void navigateUp(@NonNull Activity activity) {
+        Intent upIntent = NavUtils.getParentActivityIntent(activity);
+        if (upIntent == null) {
+            activity.finish();
+        } else if (NavUtils.shouldUpRecreateTask(activity, upIntent)) {
+            // This activity is NOT part of this app's task, so create a new task
+            // when navigating up, with a synthesized back stack.
+            TaskStackBuilder.create(activity)
+                    // Add all of this activity's parents to the back stack
+                    .addNextIntentWithParentStack(upIntent)
+                    // Navigate up to the closest parent
+                    .startActivities();
+        } else {
+            // This activity is part of this app's task, so simply
+            // navigate up to the logical parent activity.
+            NavUtils.navigateUpTo(activity, upIntent);
+        }
+    }
 }
